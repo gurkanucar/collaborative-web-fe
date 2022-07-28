ce84d402a80f742f803772ad664524bf2ac30ead
Gurkan UCAR <ucar.gurkan@hotmail.com>
2022-07-28 18:16:40 +0300

    app js cleaned

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import { TextEditor } from './components/TextEditor/TextEditor';
+import { CodePage } from './pages/CodePage/CodePage';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+        <CodePage/>
     </div>
   );
 }
