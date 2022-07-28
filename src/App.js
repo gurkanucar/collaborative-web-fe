1b0d43dbaebed0b8ceb46c51c84f71b82c227db9
Gurkan UCAR <ucar.gurkan@hotmail.com>
2022-07-28 21:31:17 +0300

    route added to app js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
-import { TextEditor } from './components/TextEditor/TextEditor';
-import { CodePage } from './pages/CodePage/CodePage';
+import logo from "./logo.svg";
+import "./App.css";
+import { TextEditor } from "./components/TextEditor/TextEditor";
+import { CodePage } from "./pages/CodePage/CodePage";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HomePage } from "./pages/HomePage/HomePage";
 
 function App() {
   return (
-    <div>
-        <CodePage/>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="project" element={<CodePage />}>
+          <Route path=":projectID" element={<CodePage />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
