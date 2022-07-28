2e70106a9bb6506bd909f677971b126f3aa34972
Gurkan UCAR <ucar.gurkan@hotmail.com>
2022-07-29 02:33:25 +0300

    app js updated

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import logo from "./logo.svg";
 import "./App.css";
-import { TextEditor } from "./components/TextEditor/TextEditor";
-import { CodePage } from "./pages/CodePage/CodePage";
+import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { HomePage } from "./pages/HomePage/HomePage";
+import { PlaygroundPage } from "./pages/PlaygroundPage/PlaygroundPage";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route path="project" element={<CodePage />}>
-          <Route path=":projectID" element={<CodePage />} />
+        <Route path="project" element={<PlaygroundPage />}>
+          <Route path=":projectID" element={<PlaygroundPage />} />
         </Route>
       </Routes>
     </BrowserRouter>
