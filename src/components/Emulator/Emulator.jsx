b0100623f0bc6070b7be31d050a80e680d3a8e58
Gurkan UCAR <ucar.gurkan@hotmail.com>
2022-07-29 02:33:52 +0300

    emulator updated

--- src/components/Emulator/Emulator.jsx
+++ src/components/Emulator/Emulator.jsx
@@ -1,39 +1,22 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import "./Emulator.css";
 export const Emulator = (props) => {
-  const [srcDoc, setSrcDoc] = useState("");
-  const { html, css, js } = props;
-
-  // const html = "<h1>Selam!</h1><br/><h1>Selam!</h1><br/><h1>Selam!</h1><br/><h1>Selam!</h1><br/><h1>Selam!</h1><br/>";
-  // const css = "";
-  // const js = "";
-
-  useEffect(() => {
-    // const timeout = setTimeout(() => {
-    setSrcDoc(`
-            <html>
-              <body>${html || ""}</body>
-              <style>${css || ""}</style>
-              <script>${js || ""}</script>
-            </html>
-          `);
-    //  }, 250);
-
-    // return () => clearTimeout(timeout);
-  }, [html, css, js]);
+  const { html, css, js } = props.values;
 
   return (
-    <div>
-      <div className="pane">
-        <iframe
-          srcDoc={srcDoc}
-          title="output"
-          sandbox="allow-modals allow-forms allow-popups allow-scripts allow-same-origin"
-          frameBorder="0"
-          width="100%"
-          height="100%"
-        />
-      </div>
+    <div className="emulator">
+      <iframe
+        srcDoc={` <html>
+        <body>${html || ""}</body>
+        <style>${css || ""}</style>
+        <script>${js || ""}</script>
+      </html>`}
+        title="output"
+        sandbox="allow-modals allow-forms allow-popups allow-scripts allow-same-origin"
+        frameBorder="0"
+        width="100%"
+        height="100%"
+      />
     </div>
   );
 };
