dfaf25dc2bee0e34930bdbb531c7fe58841a778b
Gurkan UCAR <ucar.gurkan@hotmail.com>
2022-07-29 12:01:53 +0300

    new components implemented to home page

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { v4 as uuidv4 } from "uuid";
+import { Button } from "../../components/Button/Button";
+import { InputComponent } from "../../components/InputComponent/InputComponent";
 
 import "./HomePage.css";
 
@@ -19,15 +21,20 @@ export const HomePage = () => {
   };
 
   return (
-    <div>
-      <button onClick={createNewProject}>Create New</button>
-      <div>
-        <input
-          onChange={(e) => setProjectID(e.target.value)}
-          value={projectID}
-          placeholder="project id"
-        />
-        <button onClick={openProject}>Open Project</button>
+    <div className="home_root">
+      <div className="home">
+        <span className="home_title">Collaborative HTML-CSS-JS Editor</span>
+        <br />
+        <Button text={"Create New"} onClick={createNewProject} />
+        <h3>OR</h3>
+        <div className="home_open_project">
+          <InputComponent
+            onChange={(e) => setProjectID(e.target.value)}
+            value={projectID}
+            placeholder="project id"
+          />
+          <Button text={"Open Project"} onClick={openProject} />
+        </div>
       </div>
     </div>
   );
